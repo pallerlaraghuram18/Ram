17be622986d7e14ac0353e7ebd6a09ab8f715b7d
user <pallerla.raghuram18@gmail.com>
2014-07-18 20:51:07 +0530

    my first commit

---
+++ MyProjects/src/ebayIn.java
@@ -0,0 +1,13 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+
+public class ebayIn {
+
+	public static void main(String[] args) {
+		System.out.println("OM");
+		WebDriver driver=new FirefoxDriver();
+		driver.get("http://www.ebay.in");
+		}
+
+}
